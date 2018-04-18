286bdd60d9f394016e4458aac1a880e0706cf263
sdi2 <sdi2@student.unimelb.edu.au>
2018-04-18 21:53:22 +1000

    Linkage of pages with uniform colours

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
-
+/***************************************************/
 const express = require('express');
 const app = express();
 
 const controller = require('./Controllers/controller');
 //Obtain Modal Form
 const modalForm = document.getElementById('id1');
-
-
-
 //const router = require('./routes/routes');
 //Set View engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
 //Heroku server port
 const PORT = process.env.PORT || 3000;
-
-
-//Local Data storage
-var jobs = [];
-var users =  [];
-var companies = [];
-
-
-
+/*******************************************************************/
 //Coming soon page render
 app.get('/',function(req,res){
     res.render('ComingSoon.ejs');
 });
 
 app.get('/home',function(req,res){
-    res.render('Home.html');
-})
 
+    res.render('Home.html');
+});
+var names = ["admin"];
 
 window.onclick=function(event){
 
-       res.render('ComingSoon.ejs')
+    res.render('ComingSoon.ejs')
 
-}
+};
+/***********************************************************************/
+//Local Data storage
+
+
+/******************************************************************/
 function userSignUp(){
 
 }
@@ -45,57 +40,37 @@ function companySignUp(){
 /*get data from html add to arraylist open newpage with usernamespecified*/
 }
 
-var names = new Array(6);
-var name = "admin";
-names.push(name);
-var passwords = ["admin","simon"];
+
+
+
 function login(){
-    var user = {
-        fullname: "Bob Shmaf",
-        username: "admin",
-        password: "admin"
-    };
+    var num = Math.floor(Math.random()*10)+1;
 
+        if( num >5){
+            window.location= 'userProfile.html';
+            document.getElementById("UserFullName").value = "Bob Shmif";
 
-
-    var username = document.getElementById("uname").valueOf();
-    var pword = document.getElementById("pword").valueOf();
-    console.log(username.value);
-    console.log(names[0]);
-
-        if(usernames[0] == username.value){
-            if(passwords[0].toString() == pword.toString()){
-
-                window.location = 'userProfile.html';
-                document.getElementById("UserFullName").value = "Admin Login";
-
-            }
-        }
-
-    /*
-    for(var i = 0; i<users.length; i++){
-        if(users[i].username.valueOf() == username.valueOf()){
-           if(users[i].password.valueOf() == pword.valueOf()){
-
-               window.location = 'userProfile.html';
-               document.getElementById("UserFullName").value = users[i].fullname;
-
-           }
-        }
-    }
-    for(var i = 0; i<companies.length; i++){
-        if(companies[i].username == username){
-            if(companies[i].password == pword){
-                window.location = 'companyProfile.html';
-            }
+        }else{
+            window.location = 'companyProfile.html';
+            document.getElementById("CompanyName").value = "CardBoard Enthusiasts";
 
         }
-    }
-*/
+
+
+
+
+
+
+
+
+
 
 }
 function clearContent(id){
     document.getElementById(id).value = '';
+}
+function addJob(){
+    window.location = 'JobPost.html';
 }
 
 function postJob(){
@@ -105,7 +80,7 @@ function postJob(){
         description: document.getElementById('jobDesc').valueOf(),
         email: document.getElementById('jobEmail').valueOf()
     };
-    jobs.push(job);
+    //jobs.push(job);
     window.location = 'companyProfile.html';
 }
     /* Submit and save data entered*/
@@ -114,27 +89,34 @@ function goHome(){
     window.location = 'Home.html';
 }
 function goJobs(){
-    window.location = '';
+    window.location = 'jobs.html';
 }
 function goCompanies(){
-    window.location = '';
+    window.location = 'companies.html';
 }
 function goTraining(){
-    window.location = 'training.HTML';
+    window.location = 'training.html';
 }
 function goExperience(){
-    window.location = 'experiences.HTML';
+    window.location = 'experiences.html';
 
 }
 function goUserSignup() {
-    window.location = 'userSignup.HTML';
-}function goToProfile(elementid){
+    window.location = 'userSignup.html';
+}
+function goCompanySignup(){
+    window.location = 'companySignup.html';
+}
+function goToProfile(elementid){
     //check username in db
     //username = document.getElementById(elementid).valueOf();
     //if user go to userPage
     window.location='userProfile.html';
     //if company go to companyPage
     //window.location='companyProfile';
+}
+function goToCompanyProfile(){
+    window.location ='companyProfile.html'
 }
 
 //For use with controller and router
