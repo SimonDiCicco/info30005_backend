bec24671e383c833939a1f86290976569f9e7cbe
sdi2 <sdi2@student.unimelb.edu.au>
2018-04-20 17:03:23 +1000

    added dummy jobs, companies and users, with alternate companies and users to be displayed on profile page when refreshed

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.get('/', function(req,res){
 /*
 Data model
 ----------
+Users---
 name: string
 interests: string
 experiences: string
@@ -30,10 +31,65 @@ jobs: [{// put details here}, {}, {}, ...]
 
 
 const fakeUsers = [{
-    name: "John Smith",
+    name: "John Shmuf",
+    username: "john1",
+    password: "shmuf",
     interests: "I love cycling, and spending time with family.",
     experiences: " I have have some experience as a sales attendant at Big W."
 }];
+const fakeJobs = [
+    {
+        title: "Pick Packer, CardBoard Enthusiasts",
+        description: "Pick items based on a checklist order, pick at your own rate, accuracy is important."
+    },
+    {
+        title: "Librarian, Hampshire Council",
+        description: "Sorting of books, magazines and DVDs. Handling customer questions about location of books within the library."
+    },
+    {
+        title: "Data Entry, Tennis Australia",
+        description: "Entry of generic sports data, based on input and real life game-play."
+    },
+    {
+        title: "HR Assistant, Ernst and Young",
+        description: "Scanning and printing of documents and council approved plans."
+    },
+
+    {
+        title: "Sales attendant, Big W",
+        description: "Facing of shelves, replenishment of stock and customer service."
+    },
+
+    {
+        title: "Full Time truck loader, CardBoard Enthusiasts",
+        description: "Assist in preparing pallets for transportation. This involves wrapping them and ensuring the contents are stable."
+    }
+
+
+
+];
+const fakeCompanies= [
+    {
+        name:"CardBoard Enthusiasts",
+        description:"Delivery quality cardboard since 1977. We provide many additional products as well. Check us out!"
+    },
+    {
+        name:"Hampshire Council",
+        description:"We put our residents first."
+    },
+    {
+        name:"Tennis Australia",
+        description:"We strive to be inviting, and focus on encouragement within the competitive world of sport."
+    },
+    {
+        name:"Ernst and Young",
+        description:"We push the boundaries of the workforce."
+    },
+    {
+        name:"Big W",
+        description:"We are focused on delivering excellent customer service."
+    }
+];
 
 
 app.get('/home', function(req,res){
@@ -61,21 +117,31 @@ app.get('/companySignup', function(req,res){
 });
 
 app.get('/jobs', function(req,res){
-    res.render('jobs.ejs');
+    res.render('jobs.ejs',{
+        jobs:fakeJobs
+    });
 });
 app.get('/jobPost', function(req,res){
     res.render('JobPost.ejs');
 });
 app.get('/companyProfile', function(req,res){
-    res.render('companyProfile.ejs');
+    var num = Math.floor(Math.random()*5);
+    res.render('companyProfile.ejs',{
+        company: fakeCompanies[num],
+        job:fakeJobs[num]
+
+    });
 });
 
-app.get('/users/:profileID', function(req, res) {
-   console.log('request for profileID ' + req.params.profileID);
+app.get('/userProfile', function(req, res) {
 
-   res.render('userProfile.ejs', {
-       user: fakeUsers[0]
-   });
+    res.render('userProfile.ejs',{
+       user: fakeUsers[0],
+    });
+
+
+
+
 });
 
 
